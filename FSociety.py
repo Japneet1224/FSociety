9fad69ed493fb8accfba230b600abd519fef34fa
Japneet1224 <japneetsingh1224@gmail.com>
2023-09-23 23:36:04 +0530

    FSociety

---
+++ FSociety.py
@@ -0,0 +1,337 @@
+import sys
+import os
+import time
+import webbrowser
+
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(10 / 100)
+
+
+print("Japneet")
+try:
+	print('\033[91m [*] F**K SOCIETY : A Penetration Testing ToolKit')
+	print("---------------------------------------------------")
+	input("\n\033[97m [*] Enter To b00T	")
+
+	def slowprint(s):
+		for c in s + '\n':
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(10 / 1000)
+	# Booting Screen ("You Can Delete This For Your Term")
+	slowprint("[*] Booting..... ")
+	time.sleep(0.4)
+	slowprint("\033[91m[*] Don't Misuse Your Power")
+	slowprint("\033[97m[*] Humans Are Most Vulnerable")
+	time.sleep(0.5)
+	slowprint("\033[91m[*] Improve, Don't Prove")
+	time.sleep(0.4)
+	slowprint("\033[92m[*] Superpower Needs To Be Practiced")
+	print("\033[98m[*] Secuirity Is Just An Illusion")
+	time.sleep(0.5)
+	slowprint("\033[91m[*] With Some Great Power Comes Great Responsibility")
+	slowprint("\033[92m[*] Be An Exception, Be An Example")
+	time.sleep(0.6)
+	slowprint("\033[93m[*] Accept Past, Move On")
+	slowprint("\033[95m[*] Good Persons Are Always Got Misused")
+	time.sleep(0.7)
+	print("\033[97m[*] Govt. Controls Our Media... Social Medias are fake")
+	slowprint("\033[91m[*] Messing Up With Pride Is An Alltime Fool Decision")
+	time.sleep(0.5)
+	slowprint("\033[92m[*] Be An Example Before An Advisor")
+	time.sleep(0.4)
+	slowprint("\033[91m[*] Time Doesn't Heal Anything, We Just Got Habituated With Situation")
+	slowprint("\033[93m[*] A Monster Is Better Than An Arrogant God")
+	time.sleep(1)
+	slowprint("\033[97m[*] Booting Completed :)")
+	time.sleep(2)
+	slowprint("\033[92m[*] Starting Tool....")
+	time.sleep(0.3)
+
+	running = True
+	while running:
+		print('''\033[97m
+		 ___ ___  ___   ___ ___ ___ _______   __
+	    | __/ __|/ _ \ / __|_ _| __|_   _\ \ / /
+		| _|\__ \ (_) | (__ | || _|  | |  \ V / 
+		|_| |___/\___/ \___|___|___| |_|   |_|
+		''')
+		print("	\033[97m=========================================================== ")
+		slowprint("\t\t\033[91mPrivacy is a myth, just like democracy ")
+		slowprint("\t\t\tDate: " + time.strftime("%d/%m/%y"))
+		print("	\033[97m=========================================================== ")
+		print('''\033[92m
+		[1] Check Anonymity
+		[2] Admin Panel Finder
+		[3] Arp-Spoofing
+		[4] Banner Grabbing
+		[5] HULK DDOS Attack
+		[6] Fake Mail
+		[7] Build False Credentials
+		[8] Gmail Hacking
+		[9] IP Scanning
+		[10] Linux Password Cracker
+		[11] Password Generator
+		[12] Nmap Port Scanner
+		[13] Wifi DDOS
+		[14] PDF Exif Tool
+		[15] Steganography
+		[16] Get Wifi Password
+		[17] Internet Speed Test
+		[18] Network Monitoring
+		[19] Suggest Me A Movie
+		[20] Anonsurf Start
+		[21] Anonsurf Stop
+		[22] Python Emulator
+		[23] Calculator
+		[24] ARP Checking
+		[25] SMS Spoofing
+		[26] Nano Text Editor
+		[27] Phishing Attack
+		[28] SMS BOMBING
+		[29] Phone Number Info Gather
+		[30] XSS Finder
+		[31] SQLI Finder
+		[32] Shodan 
+		[33] System Information
+		[34] Netdiscover
+		[35] Mac Changer\033[92m
+		\033[93m[36] Encryption-Decryption [GUI]\033[93m
+		\033[92m[37] Encrypt Your Message [CLI]
+		[38] Decrypt Your Message [CLI]
+		[39] Translator
+		[40] Terminal Emulator
+		[41] Osint\033[92m
+		\033[93m[42] HULK Browser [GUI]\033[93m
+		\033[92m[43] Email Bombing
+		[44] Social Media Video Views Automation
+		[45] Hulk's Search Engine
+		[46] Google Dorking
+		[47] Security Camera Hacking
+		\033[94m[50] Japneet Singh
+		[51] Japneet Profile
+		[52] Japneet CalcTK_Python
+		[53] Japneet Scientific Calculator
+		[54] Japneet Instagram
+		[55] Japneet Snapchat \033[94m
+		\033[95m[69] Subscribe To My Channel 
+		\033[93m[79] Pacman [GUI]\033[93m
+		\033[92m[80] Hangman
+		[81] Snake Game\033[92m
+		\033[93m[82] Python For GUI Gamers [GUI]\033[93m
+		\033[92m[83] TIC-TAC-TOE [CLI]
+		[84] Rock Paper Scissor\033[92m
+		\033[93m[85] Tetris [GUI]\033[93m
+		\033[92m[86] Chess
+		[87] Python For CLI Gamers
+		[88] FSOCIETY Banner\033[92m
+		\033[94m[89] Golang For Ethical Hackers
+		[90] Perl For Ethical Hackers
+		[91] Ruby For Ethical Hackers
+		[92] Python For Ethical Hacking
+		[93] Bash For Ethical Hackers
+		[98] Update FSociety
+		[99] What Is Fsociety
+		[00] Exit Tool
+		[??] Help\033[92m
+		''')
+		select = input("\033[95m [?] Choose Any Option : ")
+		if select == '1':
+			os.system("python tools/amianonymous.py")
+		if select == '2':
+			os.system("python tools/admin-panel-finder.py")
+		if select == '3':
+			os.system("sudo python tools/arp-spoofing.py")
+		if select == '4':
+			os.system("python tools/banner-grabbing.py")
+		if select == '5':
+			os.system("python tools/hulk.py")
+		if select == '6':
+			os.system("python tools/fakemail.py")
+		if select == '7':
+			os.system("python tools/fake-data-generator.py")
+		if select == '8':
+			os.system("python tools/hack-gmail.py")
+		if select == '9':
+			os.system("python tools/ip-scanner.py")
+		if select == '10':
+			os.system("python tools/linux_pass_cracker.py")
+		if select == '11':
+			os.system("python tools/pass-gen.py")
+		if select == '12':
+			os.system("python tools/nmap_port_scanner.py")
+		if select == '13':
+			os.system("python tools/wifi_dos_final.py")
+		if select == '14':
+			os.system("python tools/pdf-exif-tool.py")
+		if select == '15':
+			os.system("python tools/steganography.py")
+		if select == '16':
+			os.system("python tools/get_wifipass.py")
+		if select== '17':
+			os.system("python tools/internet-speed-test.py")
+		if select == '18':
+			os.system("python tools/network-monitoring.py")
+		if select== '19':
+			os.system("python tools/movie-suggesting.py")
+		if select == '20':
+			os.system("git clone https://github.com/keralahacker/anonsurf")
+			os.system("cd anonsurf && chmod +x *")
+			os.system("./installer.sh")
+			os.system("anonsurf start")
+		if select == '21':
+			os.system("anonsurf stop")
+		if select == '22':
+			os.system(" python tools/terminal-emulator.py")
+		if select == '23':
+			os.system("python tools/calculator.py")
+		if select == '24':
+			os.system(" arp -a")
+			time.sleep(3)
+		if select == '25':
+			os.system("python tools/fakesms.py")
+		if select == '26':
+			os.system("nano")
+		if select == '27':
+			os.system("python tools/pyphisher.py")
+			time.sleep(100)
+		if select == '28':
+			os.system("python tools/bomber.py")
+			time.sleep(2)
+		if select == '29':
+			os.system("python tools/phone-number-info.py")
+		if select == '30':
+			os.system("python tools/xss-vulnerability-finder.py")
+		if select == '31':
+			os.system("python tools/sqli-scanner.py")
+		if select == '32':
+			os.system("python tools/shodan-api.py")
+		if select == '33':
+			os.system("python tools/sys-info.py")
+		if select == '34':
+			os.system("netdiscover")
+			time.sleep(10)
+			input("\033[91m[-] Enter To Exit")
+		if select == '35':
+			os.system("python tools/mac-changer.py")
+		if select == '36':
+			os.system("python tools/none-read-my-code.py")
+		if select == '37':
+			os.system("python tools/encrypt.py")
+		if select == '38':
+			os.system("python tools/decrypt.py")
+		if select == '39':
+			os.system("python tools/translator.py")
+		if select == '40':
+			os.system("python tools/terminal-emulator.py")
+		if select == '41':
+			os.system("python tools/osint.py")
+		if select == '42':
+			os.system("python hulk-browser.py")
+		if select == '43':
+			os.system("python tools/email-bomber.py")
+		if select == '44':
+			os.system("python tools/automate-social_media-video-views.py")
+		if select == '45':
+			os.system("python tools/hulk-engine.py")
+		if select == '46':
+			os.system("python tools/google-dorking.py")
+		if select == '47':
+			os.system("python tools/cam-hackers.py")
+		if select == '50':
+			url= 'https://github.com/Japneet1224?tab=repositories'
+			webbrowser.open_new_tab(url)
+		if select == '51':
+			url= 'https://github.com/Japneet1224/Profile'
+			webbrowser.open_new_tab(url)
+		if select == '52':
+			url= 'https://github.com/Japneet1224/CalcTK_Python'
+			webbrowser.open_new_tab(url)
+		if select == '53':
+			url= 'https://github.com/Japneet1224/Scientific-Calculator'
+			webbrowser.open_new_tab(url)
+		if select == '54':
+			url= 'https://www.instagram.com/japneet_s.1224/'
+			webbrowser.open_new_tab(url)
+		if select == '55':
+			url= 'https://accounts.snapchat.com/accounts/snapcodes'
+			webbrowser.open_new_tab(url)
+		if select == '69':
+			url= 'https://www.youtube.com/channel/UC4CNPhxVXPcwv5MqQE5emww'
+			webbrowser.open_new_tab(url)
+		if select == '79':
+			os.system("python tools/pacman.py")
+		if select == '80':
+			os.system("python tools/hangman.py")
+		if select == '81':
+			os.system("python tools/snake.py")
+		if select == '82':
+			url= 'https://github.com/r3dhulk/python-for-gui-gamers'
+			webbrowser.open_new_tab(url)
+		if select == '83':
+			os.system("python tools/tic-tac-toe-cli.py")
+			time.sleep(1)
+		if select == '84':
+			os.system("python tools/rps.py")
+		if select == '85':
+			os.system("python tools/tetris.py")
+		if select == '86':
+			os.system("python tools/chess.py")
+		if select == '87':
+			os.system("python tools/dunkin-donut.py")
+		if select == '88':
+			os.system("python tools/fsociety-logo.py")
+		if select == '89':
+			url= 'https://github.com/R3DHULK/golang-for-ethical-hackers'
+			webbrowser.open_new_tab(url)
+		if select == '90':
+			url= 'https://github.com/R3DHULK/perl-for-ethical-hackers'
+			webbrowser.open_new_tab(url)
+		if select == '91':
+			url= 'https://github.com/R3DHULK/ruby-for-ethical-hackers'
+			webbrowser.open_new_tab(url)
+		if select == '92':
+			url= 'https://github.com/R3DHULK/python-for-ethical-hacking'
+			webbrowser.open_new_tab(url)
+		if select == '93':
+			url='https://github.com/R3DHULK/bash-for-ethical-hackers'
+			webbrowser.open_new_tab(url)
+		if select == '98':
+			os.system("chmod +x tools/update.sh")
+			os.system("./tools/update.sh")
+		if select == '99':
+			input("\033[94m\n [+] Press Enter To Exit")
+		if select=='00':
+			input("\033[97m [?] You Sure To Exit (default=Y)?")
+			time.sleep(2)
+			slowprint("\033[97m [*] Thank You, Visit Again...")
+			time.sleep(1)
+			sys.exit()
+		if select=='?' or select=="??":
+			slowprint("\033[92m [*] 1-47 Is Main Tools")
+			time.sleep(0.5)
+			slowprint(" [*] 69-77 Is Self-Promotion")
+			time.sleep(0.5)
+			slowprint(" [*] Yellow Marked Options Are For Desktop Users")
+			time.sleep(1)
+			slowprint(" [*] Blue Marked Options Are My Best Projects")
+			time.sleep(0.5)
+			slowprint(" [*] 78-87 -Enjoy My Games")
+			time.sleep(0.5)
+			slowprint(" [*] Support This Project")
+			time.sleep(0.5)
+			input("\n [*] Enter To Go Back")
+			time.sleep(1)
+		else:
+			os.system("clear")
+except KeyboardInterrupt:
+	print("\n\033[91m [-] Exiting.....")
+	time.sleep(1)
+	os.system("clear")
+	print("\033[97m [*] Thank You, Visit Again...")
+	time.sleep(1)
+	sys.exit()
